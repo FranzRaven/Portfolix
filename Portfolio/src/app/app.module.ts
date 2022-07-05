ebbe41484878ee9800fe426333e2404619df3715
FranzRaven <francisco.reven@gmail.com>
2022-07-05 20:14:08 +0000

    separatio components

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { ExpComponent } from './components/exp/exp.component';
+import { EduComponent } from './components/edu/edu.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjComponent } from './components/proj/proj.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    ExpComponent,
+    EduComponent,
+    SkillsComponent,
+    ProjComponent
   ],
   imports: [
     BrowserModule,
